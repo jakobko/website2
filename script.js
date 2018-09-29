4fb299f8d789a36a9dc5d6470a2a8636fac54ac3
jakobko <jakobko@matnat.student.uio.no>
2018-09-29 18:39:48 +0200

    started work on transitions

---
+++ script.js
@@ -0,0 +1,10 @@
+/*jQuery("#flexcontainerTop").click(function () {
+      jQuery(this).toggleClass("transition");
+      console.log("Her er jeg");
+});*/
+
+function portfolioBPress() {
+  jQuery("#flexcontainerTop").toggleClass("transition");
+  jQuery("#flexcontainerPortfolio").toggleClass("visible");
+  console.log("Her er jeg");
+}
